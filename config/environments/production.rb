8e76dc87077c9504d5384369357569de6eb837ea
Raul Mantilla <raulmantilla@MacBook-Pro-de-Raul.local>
2016-07-22 11:55:43 -0500

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.logger = Logger.new(STDOUT) 
   config.logger.level = Logger::DEBUG
+  config.assets.debug = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -48,7 +49,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  #config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
